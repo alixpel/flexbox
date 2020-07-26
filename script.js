3ad1965a4642fe9adbf70c3bfa994246aa07e2ba
Alix Pelletier <alixpelletierpro@gmail.com>
2020-07-26 19:08:16 +0200

    add flex-align function

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ function makeSpaceAround() {
 }
 
 function makeSpaceEvenly() {
-  document.querySelector('body').classList.remon('flex-start');
+  document.querySelector('body').classList.remove('flex-start');
   document.querySelector('body').classList.remove('flex-end');
   document.querySelector('body').classList.remove('center');
   document.querySelector('body').classList.remove('space-between');
@@ -78,7 +78,49 @@ function makeSpaceEvenly() {
   document.querySelector('body').classList.add('space-evenly');
 }
 
+// ALIGN-ITEMS
 
+function makeAlignFlexStart() {
+  document.querySelector('body').classList.add('align-flex-start')
+  document.querySelector('body').classList.remove('align-flex-end')
+  document.querySelector('body').classList.remove('align-center')
+  document.querySelector('body').classList.remove('align-stretch')
+  document.querySelector('body').classList.remove('align-baseline')
+}
+
+function makeAlignFlexEnd() {
+  document.querySelector('body').classList.remove('align-flex-start')
+  document.querySelector('body').classList.add('align-flex-end')
+  document.querySelector('body').classList.remove('align-center')
+  document.querySelector('body').classList.remove('align-stretch')
+  document.querySelector('body').classList.remove('align-baseline')
+}
+
+function makeAlignCenter() {
+  document.querySelector('body').classList.remove('align-flex-start')
+  document.querySelector('body').classList.remove('align-flex-end')
+  document.querySelector('body').classList.add('align-center')
+  document.querySelector('body').classList.remove('align-stretch')
+  document.querySelector('body').classList.remove('align-baseline')
+}
+
+function makeAlignStretch() {
+  document.querySelector('body').classList.remove('align-flex-start')
+  document.querySelector('body').classList.remove('align-flex-end')
+  document.querySelector('body').classList.remove('align-center')
+  document.querySelector('body').classList.add('align-stretch')
+  document.querySelector('body').classList.remove('align-baseline')
+}
+
+function makeAlignBaseline() {
+  document.querySelector('body').classList.remove('align-flex-start')
+  document.querySelector('body').classList.remove('align-flex-end')
+  document.querySelector('body').classList.remove('align-center')
+  document.querySelector('body').classList.remove('align-stretch')
+  document.querySelector('body').classList.add('align-baseline')
+}
+
+// document.querySelector('.flex-direction-row-button').addEventListener(makeRow);
 document.querySelector('.flex-direction-row-button').addEventListener('click', makeRow);
 document.querySelector('.flex-direction-col-button').addEventListener('click', makeCol);
 
@@ -91,3 +133,9 @@ document.querySelector('.center-button').addEventListener('click', makeCenter);
 document.querySelector('.space-between-button').addEventListener('click', makeSpaceBetween);
 document.querySelector('.space-around-button').addEventListener('click', makeSpaceAround);
 document.querySelector('.space-evenly-button').addEventListener('click', makeSpaceEvenly);
+
+document.querySelector('.align-flex-start-button').addEventListener('click', makeAlignFlexStart)
+document.querySelector('.align-flex-end-button').addEventListener('click', makeAlignFlexEnd)
+document.querySelector('.align-center-button').addEventListener('click', makeAlignCenter)
+document.querySelector('.align-stretch-button').addEventListener('click', makeAlignStretch)
+document.querySelector('.align-baseline-button').addEventListener('click', makeAlignBaseline)
